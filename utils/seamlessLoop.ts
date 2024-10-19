d6a36533977562807ad359e838c5eb5d4de1586d
cx20012002 <cx20012002@gmail.com>
2024-10-19 23:37:53 +1300

    commit 4 fix

--- utils/seamlessLoop.ts
+++ utils/seamlessLoop.ts
@@ -40,9 +40,9 @@ export function horizontalLoop(items, config) {
   gsap.set(items, {
     // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
     xPercent: (i, el) => {
-      let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+      let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px") as string));
       xPercents[i] = snap(
-        (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
+        (parseFloat(gsap.getProperty(el, "x", "px") as string) / w) * 100 +
           gsap.getProperty(el, "xPercent")
       );
       return xPercents[i];
